b637b0469d49720c6a262138c2ef86fcae2f0f82
AfroDan91 <daniel_wordie@hotmail.co.uk>
2021-12-15 08:59:39 +0000

    initial coment blueprint completed

---
+++ quizApp.py
@@ -0,0 +1,28 @@
+#imports
+import sys
+import csv
+import random
+
+#print a description of how the app works
+
+
+
+#check to see if anything was entered when code ran in commandline 
+#e.g. quizApp.py christmas
+#this will filter the questions by genre if none selected import all csv's that are there 
+
+
+
+
+#for every row in the csv assigin  column 1 question and column2 answer and append to a list
+
+
+
+#pick randomly pick from the list of questions and request user input
+
+
+#send user input + q + a into a function that assesses if the answer is correct and adds 1 to a correct or wrong answers tally
+
+
+#when there are no more questions return the number of correct answers, wrong answers and % of correct 
+
