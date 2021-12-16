1de315a35160ae944ce0cdf924a89a26dcfbfbd6
AfroDan91 <daniel_wordie@hotmail.co.uk>
2021-12-16 11:37:45 +0000

    started logic

--- quizApp.py
+++ quizApp.py
@@ -2,14 +2,49 @@
 import sys
 import csv
 import random
-
-#print a description of how the app works
-
-
+import pandas as pd
+import glob
 
 #check to see if anything was entered when code ran in commandline 
 #e.g. quizApp.py christmas
 #this will filter the questions by genre if none selected import all csv's that are there 
+
+quizCatagory = "General Knowledge"
+questionList = []
+
+if len(sys.argv) > 1 : #check if there has been a catagory chosem
+    quizCatagory = str(sys.argv[1]) #record the name of the catagory
+    with open(f"{sys.argv[1]}.csv", newline='') as f: # open the csv of the chosen catagory
+        reader = csv.reader(f)
+        for row in reader: #loop through each row and make a list of questions
+            question = [row[0],row[1]] #get each question and answer as a list
+            questionList.append(question) #nest each list into 1 over all list
+else:
+    # path = r'C:\Users\danie\Desktop\Python training\quizApp'
+    # all_files = glob.glob(path + "/*.csv")
+
+    # li = []
+
+    # for filename in all_files:
+    #     df = pd.read_csv(filename)
+    #     li.append(df)
+    # print(li)
+
+    path = r'C:\Users\danie\Desktop\Python training\quizApp'
+    all_files = glob.glob(path + "/*.csv")
+
+    li = []
+
+    for filename in all_files:
+        df = pd.read_csv(filename)
+        li.append(df)
+    print(li)
+
+
+
+#print a description of how the app works
+print("Welcome to the quiz!\n")
+print(f"You have chosen to do the {quizCatagory} quiz. Good Luck!")
 
 
 
