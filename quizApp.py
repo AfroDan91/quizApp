7fb100e1efaeadc16bbb16c76af49e1b70c069e9
AfroDan91 <daniel_wordie@hotmail.co.uk>
2021-12-17 17:36:21 +0000

    changed category selection method

--- quizApp.py
+++ quizApp.py
@@ -1,60 +1,66 @@
 #imports
-import sys
 import csv
 import random
-import pandas as pd
 import glob
 
-quizCategory = "General Knowledge"
+quizCategory = ""
 questionList = []
-
- #check if there has been a category chosen
-if len(sys.argv) > 1 : #category chosen
-    quizCategory = str(sys.argv[1]) #record the name of the category
-    with open(f"{sys.argv[1]}.csv", newline='') as f: # open the csv of the chosen category
-        reader = csv.reader(f)
-        for row in reader: #loop through each row and make a list of questions
-            question = [row[0],row[1]] #get each question and answer as a list
-            questionList.append(question) #nest each list into 1 over all list
-    print(questionList)
-
-else: #no category chosen
-    path = r'C:\Users\danie\Desktop\Python training\quizApp' #sets path to location of csv files
-    all_files = glob.glob(path + "/*.csv") #creates list of csv files
-
-    for filename in all_files: #for each file
-        with open(filename, newline='') as f: #open the csv
-            reader = csv.reader(f)
-            for row in reader: #loop through each row and make a list of questions
-                question = [row[0],row[1]] #get each question and answer as a list
-                questionList.append(question) #nest each list into 1 over all list
-
-def mark(panswer, canswer): #this function evluates if the answer is correct 
-    if canswer.lower() == panswer.lower():  #if correct answer is same as users answer
-        return "correct"
-    else:
-        return "incorrect"
 
 #print a description of how the app works
 print("Welcome to the quiz!\n")
-numberOfQuestions = input("How many questions would you like? ")
-print(f"You have chosen to do the {quizCategory} quiz. Good Luck!\n")
+
+path = r'C:\Users\danie\Desktop\Python training\quizApp' #sets path to location of csv files
+all_files = glob.glob(path + "/*.csv") #creates list of csv files
+
+print(f"There are {len(all_files)} categorys to choose from, they are:")
+
+for file in all_files:
+    print(file[47:].replace(".csv", "")) #prints the names of the .csv minus the directory path
+print("General Knowledge")
+quizCategory = input("to choose a catagory type its name here -> ")
+
+def createQuestionList(reader): #creates a question list from selected .csv
+    for row in reader: #loop through each row and make a list of questions
+        question = [row[0],row[1]] #get each question and answer as a list
+        questionList.append(question) #nest each list into 1 over all list
+
+#check if chosen catagory is in all files
+if quizCategory in str(all_files): #if it is
+    with open(f"{quizCategory}.csv", newline='') as f: # open the csv of the chosen category
+        reader = csv.reader(f)
+        createQuestionList(reader) #turn it into a question list
+else: #if not
+    quizCategory = "General Knowledge"
+    for filename in all_files: #take all available questions
+        with open(filename, newline='') as f: #open the csv
+            reader = csv.reader(f)
+            createQuestionList(reader) #turn them into a question list
+
+numberOfQuestions = input(f"There are {len(questionList)} questions in that category, how many {quizCategory} questions would you like to do? ")
+print(f"You have chosen to do {numberOfQuestions} in the {quizCategory} category. Good Luck!\n")
+
+
+
+def mark(panswer, canswer): #this function evluates if the answer is correct 
+    if canswer.lower() == panswer.lower():  #if correct answer is same as users answer
+        return "Correct"
+    else:
+        return "Incorrect"
 
 shuffledQuestions = random.sample(questionList, int(numberOfQuestions)) #creates a randomised list from all questions in questionList
 
 right = 0
-wrong = 0
 
-
-for question in shuffledQuestions.copy(): 
-    print(question[0])
-    playerAnswer = input("")
-    result = mark(playerAnswer, question[1])
-    print(result)
-    if "in" in result:
-        wrong += 1
-    else:
+for question in shuffledQuestions.copy(): #for each question in the radomised list of questions
+    print(question[0]) #ask the question
+    playerAnswer = input("") #take the users answer
+    result = mark(playerAnswer, question[1]) #check if given answer is correclt
+    print(result) #print if the answer is correct or not
+    if "In" not in result:
         right += 1
-percent = round((right / int(len(shuffledQuestions)))* 100, 2)
+    else:
+        print(f"The correct answer was: {question[1]}\n")
+
+percent = round((right / int(len(shuffledQuestions)))* 100, 2) #creates a right/wrong %
 
 print(f"You got {right} questions right out of {len(shuffledQuestions)}. Thats {percent}%" )
