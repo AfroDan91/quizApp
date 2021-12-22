bf728dfa3afa31e4e81593e7f54f8628cd7a1366
AfroDan91 <daniel_wordie@hotmail.co.uk>
2021-12-22 10:28:58 +0000

    fixed spelling issues in comments.

--- quizApp.py
+++ quizApp.py
@@ -13,20 +13,20 @@ print("Welcome to the quiz!\n")
 path = r'C:\Users\danie\Desktop\Python training\quizApp' #sets path to location of csv files
 all_files = glob.glob(path + "/*.csv") #creates list of csv files
 
-print(f"There are {len(all_files)} categorys to choose from, they are:")
+print(f"There are {len(all_files)} categories to choose from, they are:")
 
 
 for file in all_files:
     print(file[47:].replace(".csv", "")) #prints the names of the .csv minus the directory path
 print("General Knowledge")
-quizCategory = input("to choose a catagory type its name here -> ")
+quizCategory = input("to choose a category type its name here -> ")
 
 def createQuestionList(reader): #creates a question list from selected .csv
     for row in reader: #loop through each row and make a list of questions
         question = [row[0],row[1]] #get each question and answer as a list
         questionList.append(question) #nest each list into 1 over all list
 
-#check if chosen catagory is in all files
+#check if chosen category is in all files
 if quizCategory in str(all_files): #if it is
     with open(f"{quizCategory}.csv", newline='') as f: # open the csv of the chosen category
         reader = csv.reader(f)
@@ -42,7 +42,7 @@ else: #if not
 numberOfQuestions = input(f"There are {len(questionList)} questions in that category, how many {quizCategory} questions would you like to do? ")
 print(f"You have chosen to do {numberOfQuestions} in the {quizCategory} category. Good Luck!\n")
 
-def mark(panswer, canswer): #this function evluates if the answer is correct 
+def mark(panswer, canswer): #this function evaluates if the answer is correct 
     if canswer.lower() == panswer.lower():  #if correct answer is same as users answer
         return "Correct"
     else:
@@ -52,10 +52,10 @@ shuffledQuestions = random.sample(questionList, int(numberOfQuestions)) #selects
 
 right = 0
 
-for question in shuffledQuestions.copy(): #for each question in the radomised list of questions
+for question in shuffledQuestions.copy(): #for each question in the randomized list of questions
     print(question[0]) #ask the question
     playerAnswer = input("") #take the users answer
-    result = mark(playerAnswer, question[1]) #check if given answer is correclt
+    result = mark(playerAnswer, question[1]) #check if given answer is correct
     print(result) #print if the answer is correct or not
     if "In" not in result:
         right += 1
